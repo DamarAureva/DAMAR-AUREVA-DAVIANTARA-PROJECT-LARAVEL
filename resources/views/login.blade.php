2ccfb4b299822f53ead3d08986f82d909b09f793
DamarAureva <damaraurevadaviantara@gmai.com>
2024-10-16 10:32:53 +0700

    add login and index

---
+++ resources/views/login.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="asset/css/style.css">
+</head>
+<body>
+    <form id="loginForm">
+        <div>
+            <label for="username">Username :</label>
+            <input type="text" name="username" id="username">
+        </div>
+        <div>
+            <label for="password">Password :</label>
+            <input type="text" name="password" id="password">
+        </div>
+        <button type="submit">LOGIN</button>
+    </form>
+
+    <script src="asset/js/login.js"></script>
+</body>
+</html>
+
